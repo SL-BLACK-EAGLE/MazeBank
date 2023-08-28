dc7e91a46acb09c09396e1d1f2bdec22b02ef089
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-08-28 22:02:41 +0530

    add admin menu

---
+++ src/main/java/com/axs/pos/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,10 @@
+package com.axs.pos.Controllers.Admin;
+
+import javafx.scene.control.Button;
+
+public class AdminMenuController {
+    public Button create_client_btn;
+    public Button clients_btn;
+    public Button deposit_btn;
+    public Button logout_btn;
+}
