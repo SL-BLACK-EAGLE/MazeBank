d850ccf87fefffe4433ee28c0e69d4048e016842
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-08-28 22:05:45 +0530

    admin menu controller

--- src/main/java/com/axs/pos/Controllers/Admin/AdminMenuController.java
+++ src/main/java/com/axs/pos/Controllers/Admin/AdminMenuController.java
@@ -1,10 +1,19 @@
 package com.axs.pos.Controllers.Admin;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class AdminMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
     public Button create_client_btn;
     public Button clients_btn;
     public Button deposit_btn;
     public Button logout_btn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
