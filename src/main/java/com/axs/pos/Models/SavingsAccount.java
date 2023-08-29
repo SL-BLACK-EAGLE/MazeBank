96313a78e87c751d41a52f96c037acc544e74796
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-08-29 23:17:00 +0530

    Add SavingsAccount class for encapsulating information

---
+++ src/main/java/com/axs/pos/Models/SavingsAccount.java
@@ -0,0 +1,23 @@
+package com.axs.pos.Models;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class SavingsAccount extends Account {
+    // The withdrawal limit from the savings
+    private final DoubleProperty withdrawalLimit;
+
+    public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit) {
+        super(owner, accountNumber, balance);
+        this.withdrawalLimit = new SimpleDoubleProperty(this, "Withdrawal Limit", withdrawalLimit);
+    }
+
+    public DoubleProperty withdrawalLimitProp() {
+        return withdrawalLimit;
+    }
+
+    @Override
+    public String toString() {
+        return accountNumberProperty().get();
+    }
+}
