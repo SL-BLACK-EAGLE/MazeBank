215301e6289512e1489537aad7b08e87e618d135
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-08-29 20:37:17 +0530

    Add Icon to window and make resizable -false

--- src/main/java/com/axs/pos/Views/ViewFactory.java
+++ src/main/java/com/axs/pos/Views/ViewFactory.java
@@ -6,6 +6,7 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -150,6 +151,8 @@ public class ViewFactory {
         }
         Stage stage = new Stage();
         stage.setScene(scene);
+        stage.getIcons().add(new Image(String.valueOf(getClass().getResource("/Images/icon.png"))));
+        stage.setResizable(false);
         stage.setTitle("Maze Bank");
         stage.show();
     }
