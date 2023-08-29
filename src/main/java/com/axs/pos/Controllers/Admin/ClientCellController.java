c7a4cd442e82d79ce4476af5986bf6bc561aa4b4
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-08-29 14:43:34 +0530

    Admin view - Client Cell create, css

---
+++ src/main/java/com/axs/pos/Controllers/Admin/ClientCellController.java
@@ -0,0 +1,23 @@
+package com.axs.pos.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+    public Label fName_lbl;
+    public Label lName_lbl;
+    public Label pAddress_lbl;
+    public Label ch_acc_lbl;
+    public Label sv_acc_lbl;
+    public Label date_lbl;
+    public Button delete_btn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
